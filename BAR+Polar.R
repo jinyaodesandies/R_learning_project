49fa2635fef2969b8fda30b499d177b5de5db0c2
jinyaodesandies <60386838+jinyaodesandies@users.noreply.github.com>
2025-05-26 17:58:13 +0800

    Polar graphs are just circular bar graphs: cool!

---
+++ BAR+Polar.R
@@ -0,0 +1,8 @@
+bar <- ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut, fill = cut),
+           show.legend = FALSE,
+           width = 1)+
+  theme(aspect.ratio = 1)+
+  labs(x=NULL, y= NULL)
+bar+coord_flip()
+bar+coord_polar()

382fd8f2cc51977063105854d2c03b59d8ea7a68
jinyaodesandies <jinyaodesandies@gmail.com>
2025-06-16 07:41:13 -0700

    Finding out who stole christmas through filter()

--- Filter.R
+++ Filter.R
@@ -1,5 +1,6 @@
 worst <- flights |>
-  filter(dep_delay>=120)|>
+  filter(dep_delay>=120& month == 12 & day %in% c(20,21,22,23,24))
 
 ggplot(data = worst)+
   geom_bar(aes(x= carrier, fill = carrier))
+#ExpressJet Airlines Inc. stole christmas as discovered through "airline"
